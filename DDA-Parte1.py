d51b6f540d9bdfb00d7191f14886b8f50a22b27c
ssnot <adrianleon3005@gmail.com>
2026-04-13 21:21:36 -0500

    Variables longitud y altura
    Creación y lectura de variables longitud y altura, según los parámetros necesarios.

--- DDA-Parte1.py
+++ DDA-Parte1.py
@@ -1,1 +1,6 @@
-
+longitud = float(input("Ingrese la longitud del dique, L(m): "))
+while longitud < 1.5 or longitud > 3.5:
+    longitud = float(input("La longitud debe estar en el intervalo [1,50; 3,50]: "))
+altura = float(input("Ingrese la altura del nivel del fluido, H(m): "))
+while altura < 1 or altura > 4.8:
+    altura = float(input("La altura debe estar en el intervalo [1,00; 4,80]: "))
